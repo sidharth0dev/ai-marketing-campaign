0d3616540952dc64cfde5c116e8078f9e3ab7399
Sidharth Dev <sidharth0dev@gmail.com>
2025-11-17 17:01:05 +0400

    Fix: Correct types for ThemeProvider

--- frontend/src/components/theme-provider.tsx
+++ frontend/src/components/theme-provider.tsx
@@ -1,31 +1,10 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import * as React from "react"
 
-interface ThemeProviderProps {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
-}
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({
-  children,
-  attribute = 'class',
-  defaultTheme = 'dark',
-  enableSystem = false,
-  disableTransitionOnChange = true,
-}: ThemeProviderProps) {
-  return (
-    <NextThemesProvider
-      attribute={attribute}
-      defaultTheme={defaultTheme}
-      enableSystem={enableSystem}
-      disableTransitionOnChange={disableTransitionOnChange}
-    >
-      {children}
-    </NextThemesProvider>
-  )
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
